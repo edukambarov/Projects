1210cfced88dfdefbb06de95736b4395500f2e5b
edukambarov <edukambarovgb@yandex.ru>
2023-05-23 11:38:37 +0300

    Task12 is done

--- Task12/Program.cs
+++ Task12/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 12. Напишите программу, которая будет принимать на
+// вход два числа и выводить, является ли первое число
+// кратным второму. Если первое число не кратно
+// второму, то программа выводит остаток от деления.
+// 34, 5 -> не кратно, остаток 4
+// 16, 4 -> кратно
+
+Console.Write("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int result = Remain(num1, num2);
+string printLine = result == 0 ? "Кратно" : $"не кратно, остаток {result}";
+Console.WriteLine(printLine);
+
+int Remain(int number1, int number2)
+{
+    int res = number1 % number2;
+
+    return res;
+}
