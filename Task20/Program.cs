0f0b9f19fed46b0723f439f180db2eb39e717db9
edukambarov <edukambarovgb@yandex.ru>
2023-05-26 12:02:04 +0300

    Tasks20&22 are done

---
+++ Task20/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 20: Напишите программу, которая
+// принимает на вход координаты двух точек и
+// находит расстояние между ними в 2D
+// пространстве.
+// A (3,6); B (2,1) -> 5,09
+// A (7,-5); B (1,-1) -> 7,21
+
+double d = Math.Sqrt(5);
+double num = 5.099874453;
+double dRound = Math.Round(num, 2, MidpointRounding.ToZero);
+System.Console.WriteLine(dRound);
