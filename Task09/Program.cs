08aed21dbc1840d3091d0020f411e2a12605f0b8
edukambarov <edukambarovgb@yandex.ru>
2023-05-26 10:14:10 +0300

    Morning commit

--- Task09/Program.cs
+++ Task09/Program.cs
@@ -1,7 +1,7 @@
 ﻿// 9. Напишите программу, которая A 
 // выводит
 // случайное число из отрезка [10, 99] и C показывает
-// D наибольшую цифру числа.
+// D наибольшую цифру числа.            
 // 78 -> 8
 // 12-> 2
 // 85 -> 89. 
@@ -9,7 +9,7 @@
 int number = new Random().Next(10, 100);
 Console.WriteLine($"Случайное число тз отрезка 10 - 99 -> {number}");
 
-// int firstDigit = num / 10;
+// int firstDigit = num / 10;       
 // int secondDigit = num % 10;
 //if (firstDigit > secondDigit)
 // {
