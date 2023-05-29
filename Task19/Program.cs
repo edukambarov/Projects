186e0f531523b80c6cc151d1ae404528c7eb8ee2
edukambarov <edukambarovgb@yandex.ru>
2023-05-29 23:03:00 +0300

    Task21 is done

---
+++ Task19/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, 
+// является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+int number = FromStringToInt("Введите число: ");
+int length = (Console.ReadLine());
+
+
+int ReverseNumber(int number);
+
+    int numberStart = number / 10;
+    int numberEnd = number % 10;
+
+while (iEnd>iStart)
+{
+    iEnd = iEnd - 1;
+    iStart = iStart + 1;
+    break;
+}
+
+return number
+
+int FromStringToInt(string message)
+{
+    System.Console.Write(message);
+    string value = Console.ReadLine();
+    int result1 = Convert.ToInt32(value);
+    return result1;
+}
+
+    
+
--- Task21/Program.cs
+++ Task21/Program.cs
@@ -1,9 +1,7 @@
-﻿// Задача 20: Напишите программу, которая
-// принимает на вход координаты двух точек и
-// находит расстояние между ними в 2D
-// пространстве.
-// A (3,6); B (2,1) -> 5,09
-// A (7,-5); B (1,-1) -> 7,21
+﻿// Напишите программу, которая принимает на вход координаты двух точек и
+//  находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
 
 Console.WriteLine("Введите значение X для первой точки: ");
 int x1Coordinate = Convert.ToInt32(Console.ReadLine());
@@ -11,22 +9,29 @@ int x1Coordinate = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите значение Y для первой точки: ");
 int y1Coordinate = Convert.ToInt32(Console.ReadLine());
 
+Console.WriteLine("Введите значение z для первой точки: ");
+int z1Coordinate = Convert.ToInt32(Console.ReadLine());
+
 Console.WriteLine("Введите значение X для второй точки: ");
 int x2Coordinate = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("Введите значение Y для второй точки: ");
 int y2Coordinate = Convert.ToInt32(Console.ReadLine());
 
-double distance = Distance(x1Coordinate, y1Coordinate, x2Coordinate, y2Coordinate);
+Console.WriteLine("Введите значение Z для второй точки: ");
+int z2Coordinate = Convert.ToInt32(Console.ReadLine());
+
+double distance = Distance(x1Coordinate, y1Coordinate, z1Coordinate, x2Coordinate, y2Coordinate, z2Coordinate);
 double dRound = Math.Round(distance, 2, MidpointRounding.ToZero);
 
 Console.WriteLine($"Расстояние между заданными точками = {dRound}");
 
-double Distance(int x1, int y1, int x2, int y2)
+double Distance(int x1, int y1, int z1, int x2, int y2, int z2)
 {
     int xDiff = x2 - x1;
     int yDiff = y2 - y1;
-    int sum = xDiff*xDiff + yDiff*yDiff;
+    int zDiff = z2 - z1;
+    int sum = xDiff*xDiff + yDiff*yDiff + zDiff*zDiff;
     double result = Math.Sqrt(sum);
     
     return result;
