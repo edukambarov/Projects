c365ce7517ee3334eed8a90d9a10d8bb3eeef866
edukambarov <edukambarovgb@yandex.ru>
2023-05-30 12:04:49 +0300

    Task30 is done

---
+++ Task30/Program.cs
@@ -0,0 +1,42 @@
+﻿// int [] array = new int[8]; //1 способ
+// int [] array1 = new int[8] {2,5,6,5,7,9,9,6}; 
+// //                          0,1,2,3,4,5,6,7
+// int [] array2 = new int[] {2,5,6,5,7,9,9,6};
+// int [] array3 = {2,5,6,5,7,9,9,6};
+
+// var array4 = new int [8];
+// var rnd = new Random();
+// var res = 45/7 + 34 - 7/13
+
+// array[7] = 7;
+// int size = array.Length; //8
+
+// for (int i=0; 1<size; i++)
+// {
+//     array[i] = i + i;
+//     Console.Writeline(array[i] + "");
+// }
+
+// Задача 30: Напишите программу, которая
+// выводит массив из 8 элементов, заполненный
+// нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+
+int[] array = new int[8]; // 0 1 2 3 4 5 6 7
+FillArray(array);
+PrintArray(array);
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.WriteLine(arr[i]);
+    }
+}
+void FillArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(0,2);
+    }
+}
