ba80b90a33ca0132833be37d367ba932ef5fb674
edukambarov <edukambarovgb@yandex.ru>
2023-05-30 11:05:45 +0300

    Task26 2nd solution

--- Task26/Program.cs
+++ Task26/Program.cs
@@ -9,27 +9,16 @@ int DigitsInNumber(int num)
     int digits = 0;
 
     if (num == 0) return 1;
-    else if (num > 0)
-    {
-        while (num > 0)
+        while (num != 0)
         {
             digits++;
             num /= 10;
         }
-        
-    }
-    else if (num < 0)
-    {
-        while (num < 0)
-        {
-            digits++;
-            num /= 10;
-        }
-    }
     return digits;
 }
 
 Console.Write("Enter a number: ");
 int number = Convert.ToInt32(Console.ReadLine());
+int digitsInNUmber = DigitsInNumber(number);
 
-Console.WriteLine($"{number} contains {DigitsInNumber(number)} digits");
+Console.WriteLine($"{number} contains {digitsInNUmber} digits");
