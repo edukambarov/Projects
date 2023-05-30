3e17b33d6bc856e6ece7e7cf55d2ce80fc13588e
edukambarov <edukambarovgb@yandex.ru>
2023-05-30 12:28:07 +0300

    Tasks 28 & 30 are done

--- Task30/Program.cs
+++ Task30/Program.cs
@@ -35,8 +35,9 @@ void PrintArray(int[] arr)
 }
 void FillArray(int[] arr)
 {
+    Random rnd = new Random();
     for (int i = 0; i < arr.Length; i++)
     {
-        arr[i] = new Random().Next(0,2);
+        arr[i] = rnd.Next(0,2);
     }
 }
