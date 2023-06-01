ff0b9efc265b802cf234fe2b37e32aca41e52dda
edukambarov <edukambarovgb@yandex.ru>
2023-06-01 17:18:27 +0300

    Task27 is done

---
+++ Task27/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число
+// и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int number = FromStringToInt("Введите целое число: ");
+int sumOfDigits = SumOfDigits(number);
+Console.WriteLine($"Cумма цифр заданного числа {number} составляет {sumOfDigits}.");
+
+int FromStringToInt(string message)
+{
+    System.Console.Write(message);
+    string value = Console.ReadLine();
+    int result1 = Convert.ToInt32(value);
+    return result1;
+}
+
+int SumOfDigits(int num)
+{
+    int sum = 0;
+    for (int i = 0; num > 0; i++)
+    {
+        sum = sum + num % 10;
+        num = num / 10;       
+    }
+    return sum;
+}
