abeb7b8dae026fcb05c47e52326192d0bfb3d26c
edukambarov <edukambarovgb@yandex.ru>
2023-06-09 15:33:10 +0300

    Task52 is donee

---
+++ Task52/Program.cs
@@ -0,0 +1,70 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+int max = 25;
+int min = -25;
+int rows = 4;
+int columns = 5;
+int[,] array2d = CreateMatrixRndInt(rows, columns, min, max);
+PrintMatrix(array2d);
+double[] result = FillArrayWithColumnsMeans(array2d, min, max);
+Console.WriteLine();
+PrintArrayDouble(result);
+
+double[] FillArrayWithColumnsMeans(int[,] matrix, int min, int max)
+{
+    double[] arr = new double[array2d.GetLength(1)];
+    Random rnd = new Random();
+        for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        double sum = 0;
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            sum = sum + matrix[i,j];
+        }
+        arr[j] = sum / matrix.GetLength(0);
+    }
+    return arr;
+}
+
+void PrintArrayDouble(double[] arr, string sep = "; ")
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{Math.Round(arr[i], 1)}{sep}");
+        else Console.Write($"{Math.Round(arr[i], 1)}");
+    }
+    Console.Write("]");
+}
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max) // rows = 3, columns = 4
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],6}");
+        }
+        Console.WriteLine();
+    }
+}
