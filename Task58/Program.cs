161dc9d4d11ecb40a10d7aa5d311dd581e18f498
edukambarov <edukambarovgb@yandex.ru>
2023-06-13 23:20:08 +0300

    Task58 is partly done

---
+++ Task58/Program.cs
@@ -0,0 +1,75 @@
+﻿// Задача 58: Задайте две матрицы. 
+// Напишите программу, которая будет находить произведение двух матриц.
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+// 15 18
+
+int[,] array2dA = CreateMatrixRndInt(4, 2, 1, 10);
+Console.WriteLine("Матрица А: ");
+PrintMatrix(array2dA);
+Console.WriteLine("");
+Console.WriteLine("Матрица B: ");
+int[,] array2dB = CreateMatrixRndInt(3, 3, 1, 10);
+PrintMatrix(array2dB);
+Console.WriteLine("");
+MatrixProductExists(array2dA, array2dB);
+int[,] array2dAxB = MatrixProduct(array2dA, array2dB);
+PrintMatrix(array2dAxB);
+
+
+void MatrixProductExists(int[,] matrix1, int[,] matrix2)
+{
+    if (matrix1.GetLength(1) == matrix2.GetLength(0))
+        Console.WriteLine("Произведение матрицы А на матрицу В выглядит следующим образом:");
+    else Console.WriteLine("Невозможно вычислить произвведение матрицы А на матрицу В.");
+}
+
+void MatrixProduct(int[,] matrix1, int[,] matrix2)
+{
+    if (matrix1.GetLength(1) == matrix2.GetLength(0))
+    {
+        Console.WriteLine("Произведение матрицы А на матрицу В выглядит следующим образом:");
+        int[,] matrixResult = new int[matrix1.GetLength(0), matrix2.GetLength(1)];
+        for (int m = 0; m < matrix1.GetLength(0); m++)
+        {
+            for (int n = 0; n < matrix2.GetLength(1); n++)
+            {
+                matrixResult[m, n] = 0;
+                for (int j = 0; j < matrix1.GetLength(1); j++)
+                {
+                    matrixResult[m, n] = matrixResult[m, n] + matrix1[m, j] * matrix2[j, n];
+                }
+            }
+        }
+    }
+    else Console.WriteLine("Невозможно вычислить произвведение матрицы А на матрицу В.");
+}
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max) // rows = 3, columns = 4
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],6}");
+        }
+        Console.WriteLine();
+    }
+}
