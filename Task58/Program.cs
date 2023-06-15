7c377de248af10ab32e02235b09d9fe56db44092
edukambarov <edukambarovgb@yandex.ru>
2023-06-15 08:30:44 +0300

    Comments for Task58 are cleared

--- Task58/Program.cs
+++ Task58/Program.cs
@@ -11,52 +11,60 @@ int[,] array2dA = CreateMatrixRndInt(4, 2, 1, 10);
 Console.WriteLine("Матрица А: ");
 PrintMatrix(array2dA);
 Console.WriteLine("");
-int[,] array2dB = CreateMatrixRndInt(2, 3, 1, 10);
+int[,] array2dB = CreateMatrixRndInt(3, 3, 1, 10);
 Console.WriteLine("Матрица B: ");
 PrintMatrix(array2dB);
+MatrixProductsExists(array2dA, array2dB);
 Console.WriteLine("");
 int[,] array2dAxB = MatrixProduct(array2dA, array2dB);
-PrintMatrixIfExists(array2dAxB, array2dA, array2dB);
+if (array2dA.GetLength(1) == array2dB.GetLength(0)) PrintMatrix(array2dAxB);
 
 
 int[,] MatrixProduct(int[,] matrix1, int[,] matrix2)
 {
-    int[,] matrixResult = new int[matrix1.GetLength(0), matrix2.GetLength(1)]; // [m,n] 
-    for (int m = 0; m < matrix1.GetLength(0); m++)
-    {
-        for (int n = 0; n < matrix2.GetLength(1); n++)
+    int[,] matrixResult = new int[matrix1.GetLength(0), matrix2.GetLength(1)];
+    if (matrix1.GetLength(1) == matrix2.GetLength(0))
+    {        
+        for (int m = 0; m < matrix1.GetLength(0); m++)
         {
-            matrixResult[m, n] = 0;
-            for (int j = 0; j < matrix1.GetLength(1); j++)
+            for (int n = 0; n < matrix2.GetLength(1); n++)
             {
-                matrixResult[m, n] = matrixResult[m, n] + matrix1[m, j] * matrix2[j, n];
+                matrixResult[m, n] = 0;
+                for (int j = 0; j < matrix1.GetLength(1); j++)
+                {
+                    matrixResult[m, n] = matrixResult[m, n] + matrix1[m, j] * matrix2[j, n];
+                }
             }
         }
     }
     return matrixResult;
 }
 
-int[,] CreateMatrixRndInt(int rows, int columns, int min, int max) // rows = 3, columns = 4
-{
-    int[,] matrix = new int[rows, columns];
-    Random rnd = new Random();
-    for (int i = 0; i < matrix.GetLength(0); i++)
+    int[,] CreateMatrixRndInt(int rows, int columns, int min, int max) // rows = 3, columns = 4
     {
-        for (int j = 0; j < matrix.GetLength(1); j++)
+        int[,] matrix = new int[rows, columns];
+        Random rnd = new Random();
+        for (int i = 0; i < matrix.GetLength(0); i++)
         {
-            matrix[i, j] = rnd.Next(min, max + 1);
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                matrix[i, j] = rnd.Next(min, max + 1);
+            }
         }
-    }
-    return matrix;
-}
 
-void PrintMatrixIfExists(int[,] matrix, int[,] matrix1, int[,] matrix2)
-{
-    if (matrix1.GetLength(1) != matrix2.GetLength(0))
-        Console.WriteLine("Невозможно произвести умножение матрицы А на матрицу В. Количество столбцов матрицы А должно совпадать с количеством строк матрицы В.");
-    else
+        return matrix;
+    }
+
+    void MatrixProductsExists(int[,] matrix1, int[,] matrix2)
     {
-        Console.WriteLine("Произведение матрицы А на матрицу В выглядит следующим образом:");
+        if (matrix1.GetLength(1) == matrix2.GetLength(0))
+            Console.WriteLine("Произведение матрицы А на матрицу В выглядит следующим образом:");
+        else
+            Console.WriteLine("Невозможно произвести умножение матрицы А на матрицу В. Количество столбцов матрицы А должно совпадать с количеством строк матрицы В.");
+    }
+
+    void PrintMatrix(int[,] matrix)
+    {
         for (int i = 0; i < matrix.GetLength(0); i++)
         {
             for (int j = 0; j < matrix.GetLength(1); j++)
@@ -66,16 +74,3 @@ void PrintMatrixIfExists(int[,] matrix, int[,] matrix1, int[,] matrix2)
             Console.WriteLine();
         }
     }
-}
-
-void PrintMatrix(int[,] matrix)
-{
-    for (int i = 0; i < matrix.GetLength(0); i++)
-    {
-        for (int j = 0; j < matrix.GetLength(1); j++)
-        {
-            Console.Write($"{matrix[i, j],6}");
-        }
-        Console.WriteLine();
-    }
-}
