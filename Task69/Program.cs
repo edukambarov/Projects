4de9d0e75831423ed891cbdb20d277f0084da5a0
edukambarov <edukambarovgb@yandex.ru>
2023-06-16 11:27:08 +0300

    Task69 is done

--- Task69/Program.cs
+++ Task69/Program.cs
@@ -3,3 +3,24 @@
 // A = 3; B = 5 -> 243 (3⁵)
 // A = 2; B = 3 -> 8
 
+int PowerRec(int num, int pow)
+{
+    if (pow == 0) return 1;
+    else return num * PowerRec(num, pow - 1);
+}
+
+Console.WriteLine("Введите два натуральных числа: ");
+int baseNumber = Convert.ToInt32(Console.ReadLine());
+int power = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(PowerRec(baseNumber, power));int PowerRec(int num, int pow)
+{
+    if (pow == 0) return 1;
+    return num * PowerRec(num, pow - 1);
+}
+
+Console.WriteLine("Введите два натуральных числа: ");
+int baseNumber = Convert.ToInt32(Console.ReadLine());
+int power = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(PowerRec(baseNumber, power));
